3a4a76ca412dfed75f9d2f2db85a00c00a9b5cdb
monika <monikamahanthappa@gmail.com>
2021-11-08 10:55:03 +0530

    Handle fizz for multiple of three

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,16 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ = "Fizz";
+
     public String run(int number) {
+        if (isMultipleOfThree(number)) {
+            return FIZZ;
+        }
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOfThree(int number) {
+        return number % 3 == 0;
     }
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package fizzbuzz;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,5 +16,20 @@ public class FizzBuzzTest {
     void shouldBeTwoWhenNumberIsTwo() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.run(2));
+    }
+
+    @Nested
+    class MultipleOfThree {
+        @Test
+        void shouldBeFizzWhenNumberIsThree() {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            assertEquals("Fizz", fizzBuzz.run(3));
+        }
+
+        @Test
+        void shouldBeFizzWhenNumberIsSix() {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            assertEquals("Fizz", fizzBuzz.run(6));
+        }
     }
 }
