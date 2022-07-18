baf441a2ca1e297442ba76ed2315787453e25320
monika <monikamahanthappa@gmail.com>
2022-07-18 06:01:12 +0530

    Add Fizzbuzz for any number to be number

--- src/main/java/com/tw/bootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/tw/bootcamp/fizzbuzz/FizzBuzz.java
@@ -1,24 +1,7 @@
 package com.tw.bootcamp.fizzbuzz;
 
 public class FizzBuzz {
-    public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
-
     public String run(int number) {
-        if (isMultipleOfFive(number)) {
-            return BUZZ;
-        }
-        if (isMultipleOfThree(number)) {
-            return FIZZ;
-        }
         return String.valueOf(number);
-    }
-
-    private boolean isMultipleOfFive(int number) {
-        return number % 5 == 0;
-    }
-
-    private boolean isMultipleOfThree(int number) {
-        return number % 3 == 0;
     }
 }
--- src/test/java/com/tw/bootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/tw/bootcamp/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,5 @@
 package com.tw.bootcamp.fizzbuzz;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,28 +15,5 @@ public class FizzBuzzTest {
     void shouldBeTwoWhenNumberIsTwo() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.run(2));
-    }
-
-    @Nested
-    class MultipleOfThree {
-        @Test
-        void shouldBeFizzWhenNumberIsThree() {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            assertEquals("Fizz", fizzBuzz.run(3));
-        }
-
-        @Test
-        void shouldBeFizzWhenNumberIsSix() {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            assertEquals("Fizz", fizzBuzz.run(6));
-        }
-    }
-
-    //one assertion per test
-    @Test
-    void shouldBeBuzzWhenMultipleOfFive() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertEquals("Buzz", fizzBuzz.run(10));
-        assertEquals("Buzz", fizzBuzz.run(5));
     }
 }
